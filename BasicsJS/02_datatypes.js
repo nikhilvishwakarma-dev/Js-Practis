86b6a606e02aae38824c9f173b572ca9dac0d49d
Nikhil Vishwakarma <154035036+nikhilvishwakarma-dev@users.noreply.github.com>
2026-05-29 09:49:47 +0000

    02_datatypes

---
+++ BasicsJS/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treats all JS code as newer version
+
+ //alert(3+3) // we are using nodejs, not browser, so alert is not defined. We can use console.log instead
+  
+ console.log(3+3) // this will print 6 in the console
+ console.log("Nikhil") // this will print Nikhil in the console
+
+ // primitive datatypes in JS are:
+ // number
+ // bigint
+    // string
+    // boolean
+    // null
+    // undefined
+    // symbol
+
+    console.log(typeof null) // this will print "number"
+    console.log(typeof undefined)
